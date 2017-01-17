9007c019d6eeb2163759c00a426170ba8ae26d92
Shuhao Zhang <rzsh2003@gmail.com>
2017-01-17 08:39:21 -0500

    timeline is ordered in descending order

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates_presence_of :text, message: 'must not be empty'
   validates_length_of :text, maximum: 150, too_long: 'must have at most 150 characters'
+  default_scope { order(created_at: :desc) }
 end
